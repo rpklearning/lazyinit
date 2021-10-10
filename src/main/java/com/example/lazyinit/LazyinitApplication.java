4f54396beefbf8941e0be45cc34bb5cc697f2f43
pkrajanand <pkrajanand@gmail.com>
2021-10-10 21:47:28 +1100

    Initial commit

---
+++ src/main/java/com/example/lazyinit/LazyinitApplication.java
@@ -0,0 +1,13 @@
+package com.example.lazyinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lazyinit/LazyinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lazyinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
