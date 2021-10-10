d6d7d60a23a81e891d4e2aa3959f0cd67c56a8fd
pkrajanand <pkrajanand@gmail.com>
2021-10-10 23:08:02 +1100

    a dump getter method

--- src/main/java/com/example/lazyinit/LazyinitApplication.java
+++ src/main/java/com/example/lazyinit/LazyinitApplication.java
@@ -2,12 +2,22 @@ package com.example.lazyinit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LazyinitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LazyinitApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "Raj") String name) {
+		System.out.println("Servicing the request....");
+		return String.format("Hello %s!", name);
+	}
 }
