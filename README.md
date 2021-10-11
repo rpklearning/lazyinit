cab634d2416b640396f6359d7eadecbb1314f8a6
pkrajanand <pkrajanand@gmail.com>
2021-10-11 23:13:30 +1100

    created an endpoint to generate the exception

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # lazyinit
  
+1. Run the spring-boot app
+2. Submit a POST to `http://localhost:9095/parents/lazyinitexception`
+3. Observe the LazyInitException thrown up in the server log
+4. Reason
+   1. This creates the parent object with a child object
+   2. Retrieves the parent object with the 'lazily' loaded child
+   3. Invokes a property on the lazily loaded child object. 
+5. Fix
+   1. Make these call before accessing `child.getName()`
+      1. `Child child = childService.retrieve(fetchedParent.getChild().getId());`
+
--- src/main/java/com/example/lazyinit/controller/ParentController.java
+++ src/main/java/com/example/lazyinit/controller/ParentController.java
@@ -3,19 +3,9 @@ package com.example.lazyinit.controller;
 import com.example.lazyinit.entity.Parent;
 import com.example.lazyinit.model.ParentModel;
 import com.example.lazyinit.service.ParentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.UUID;
 
 @RestController
 public class ParentController {
@@ -26,34 +16,17 @@ public class ParentController {
         this.parentService = parentService;
     }
 
-    @GetMapping("/ping")
-    public String ping() {
-        System.out.println("Servicing the request....");
-        return String.format("Server is UP");
-    }
-
-    @PostMapping("/parents")
-    public ResponseEntity<ParentModel> create(@RequestBody ParentModel parentModel) {
-        Parent parent = parentService.create(parentModel.getName(), parentModel.getChildName());
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(parent.getId())
-                .toUri();
-        return ResponseEntity.created(uri)
-                .body(toParentModel(parent));
-    }
-
-    @GetMapping("/parents/{id}")
-    public ParentModel retrieve(@PathVariable UUID id) {
-        Parent parent = parentService.retrieve(id);
-        return toParentModel(parent);
-    }
-
-    private ParentModel toParentModel(Parent parent) {
-        final ParentModel parentModel = new ParentModel();
-        parentModel.setName(parent.getName());
-        parentModel.setChildName(parent.getChild().getName()); // should throw lazy-init-exception
-        return parentModel;
+    @PostMapping("/parents/lazyinitexception")
+    public void generateLazyInitException() {
+        Parent parent = parentService.create("some", "thing");
+        Parent fetchedParent = parentService.retrieve(parent.getId());
+        fetchedParent.getChild().getName(); // should throw lazy-init-exception
+        /**
+         * How to fix the exception?
+         * 1. Autowire ChildService
+         * 2. Invoke childService.retrieve(fetchedParent.getChild().getId())
+         * 3. Call getName() on the retrieved child object
+         */
     }
 
 }
