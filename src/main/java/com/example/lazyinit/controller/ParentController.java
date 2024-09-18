afb49fc6a9a03d6eadf9db858cfa6d734365eeec
RajPK <pkrajanand@gmail.com>
2024-09-18 14:33:44 +0530

    Added more endpoints to improve readability and presentation of the use case

--- src/main/java/com/example/lazyinit/controller/ParentController.java
+++ src/main/java/com/example/lazyinit/controller/ParentController.java
@@ -1,32 +1,47 @@
 package com.example.lazyinit.controller;
 
-import com.example.lazyinit.entity.Parent;
-import com.example.lazyinit.model.ParentModel;
-import com.example.lazyinit.service.ParentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lazyinit.entity.Child;
+import com.example.lazyinit.entity.Parent;
+import com.example.lazyinit.service.ChildService;
+import com.example.lazyinit.service.ParentService;
 
 @RestController
 public class ParentController {
 
     final ParentService parentService;
+    final ChildService childService;
 
-    public ParentController(ParentService parentService) {
+    public ParentController(ParentService parentService, ChildService childService) {
         this.parentService = parentService;
+        this.childService = childService;
     }
 
-    @PostMapping("/parents/lazyinitexception")
+    /**
+     * This method will throw lazy-init-exception. Because,
+     * it tries to access the properties of child object,
+     * which is lazily loaded. Ideally, the child object should have
+     * loaded through its service before accessing its properties.
+     */
+    @GetMapping("/parents/child/name/throwsEx")
     public void generateLazyInitException() {
         Parent parent = parentService.create("some", "thing");
         Parent fetchedParent = parentService.retrieve(parent.getId());
         fetchedParent.getChild().getName(); // should throw lazy-init-exception
-        /**
-         * How to fix the exception?
-         * 1. Autowire ChildService
-         * 2. Invoke childService.retrieve(fetchedParent.getChild().getId())
-         * 3. Call getName() on the retrieved child object
-         */
     }
+    
+    @GetMapping("/parents/child/name/correctApproach")
+    public String getChildNameProperly() {
+        Parent parent = parentService.create("some", "thing");
+        Parent fetchedParent = parentService.retrieve(parent.getId());
+        
+        // loading the child object before accessing its properties
+        Child child = childService.retrieve(fetchedParent.getChild().getId());
+        
+        return child.getName(); 
+    }
+
 
 }
